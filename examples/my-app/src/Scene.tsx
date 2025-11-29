f1bf57dece89f6eb9ce8728d8ecaf5fbf428feb2
Tim Jentzsch <commits@timjen.net>
2025-11-29 09:23:21 +0100

    Expose threeJs state

--- examples/my-app/src/Scene.tsx
+++ examples/my-app/src/Scene.tsx
@@ -1,7 +1,21 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree, type RootState } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    PLAYWRIGHT_THREE?: RootState;
+  }
+}
+
 
 export default function Scene() {
+  const state = useThree();
+
+  useEffect(() => {
+    globalThis.window.PLAYWRIGHT_THREE = state;
+  }, [state]);
+
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
