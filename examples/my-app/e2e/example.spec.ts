b414f708884b93905be66651b7af16ffce857fa7
Tim Jentzsch <commits@timjen.net>
2025-11-29 15:16:40 +0100

    Add basic test with three JS

--- examples/my-app/e2e/example.spec.ts
+++ examples/my-app/e2e/example.spec.ts
@@ -1,4 +1,9 @@
 import { test, expect } from "@playwright/test";
+import { type RootState } from "@react-three/fiber";
+
+type ThreeWindow = Window & {
+  PLAYWRIGHT_THREE?: RootState;
+};
 
 test.beforeEach(async ({ page }) => {
   await page.goto("");
@@ -6,4 +11,16 @@ test.beforeEach(async ({ page }) => {
 
 test("has canvas", async ({ page }) => {
   await expect(page.locator("canvas")).toHaveCount(1);
+});
+
+test("has camera", async ({ page }) => {
+  await page.waitForFunction(
+    () => (window as ThreeWindow).PLAYWRIGHT_THREE !== undefined
+  );
+  const stateHandle = await page.evaluateHandle<RootState>(
+    (): RootState => (window as ThreeWindow).PLAYWRIGHT_THREE as RootState
+  );
+
+  const cameraId = await stateHandle.evaluate((state) => state.camera.id);
+  expect(cameraId).not.toBeUndefined();
 });
