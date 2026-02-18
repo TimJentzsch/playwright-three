842f39f413f266355457e31fd28ddda90c729f69
Tim Jentzsch <tim.jentzsch@faro.com>
2026-02-18 17:19:28 +0100

    Also allow passing locator options to scene

--- src/scene.ts
+++ src/scene.ts
@@ -1,7 +1,7 @@
 import type { JSHandle, Page } from "@playwright/test";
 import type { RootState } from "@react-three/fiber";
 import { ThreeLocator } from "./locator";
-import { ObjectLocatorApi } from "./locatorApi";
+import { LocatorOptions, ObjectLocatorApi } from "./locatorApi";
 import { Object3D } from "three";
 import { LocatorContext } from "./locatorContext";
 import { single } from "./objectGenerators";
@@ -19,25 +19,33 @@ export class Scene implements ObjectLocatorApi, LocatorContext {
   }
 
   /** @inheritdoc */
-  getByName(name: string): ThreeLocator {
-    return new ThreeLocator(this).filter({ name });
+  getByName(name: string, options?: LocatorOptions): ThreeLocator {
+    return new ThreeLocator(this, options).filter({ name });
   }
 
   /** @inheritdoc */
-  getByType(type: string): ThreeLocator {
-    return new ThreeLocator(this).filter({ type });
+  getByType(type: string, options?: LocatorOptions): ThreeLocator {
+    return new ThreeLocator(this, options).filter({ type });
   }
 
   /** @inheritdoc */
-  getByUserData<T>(key: string, value: T): ThreeLocator {
-    return new ThreeLocator(this).filter({ userData: { [key]: value } });
+  getByUserData<T>(
+    key: string,
+    value: T,
+    options?: LocatorOptions,
+  ): ThreeLocator {
+    return new ThreeLocator(this, options).filter({
+      userData: { [key]: value },
+    });
   }
 
   async roots(): Promise<Generator<Object3D>> {
     let threeHandle = this.threeHandle;
     // Lazily determine the three JS scene state, if not already done
     if (!threeHandle) {
-      await this.page.waitForFunction(() => (window as ThreeWindow).PLAYWRIGHT_THREE !== undefined);
+      await this.page.waitForFunction(
+        () => (window as ThreeWindow).PLAYWRIGHT_THREE !== undefined,
+      );
       threeHandle = await this.page.evaluateHandle<RootState>(
         (): RootState => (window as ThreeWindow).PLAYWRIGHT_THREE as RootState,
       );
