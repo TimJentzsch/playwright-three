27b8dba30b0d5a75df41dce0d487edff6d56cff1
Tim Jentzsch <tim.jentzsch@faro.com>
2026-02-23 17:17:13 +0100

    Remove need to reload page after adding scripts

--- src/test.ts
+++ src/test.ts
@@ -1,33 +1,37 @@
 import {
   test as base,
-  JSHandle,
   PlaywrightTestArgs,
   PlaywrightTestOptions,
   PlaywrightWorkerArgs,
   PlaywrightWorkerOptions,
   TestType,
 } from "@playwright/test";
-import { RootState } from "@react-three/fiber";
 import { Scene } from "./scene";
 
 const objectGeneratorsPath = new URL("./preload/objectGenerators.js", import.meta.url).pathname;
 
-type ThreeFixtures = {
-  threeHandle: JSHandle<RootState>;
-
+type ThreeTestFixtures = {
   scene: Scene;
+
+  autoLoadSceneScripts: "autoLoadSceneScripts";
 };
 
 export const test: TestType<
-  PlaywrightTestArgs & PlaywrightTestOptions & ThreeFixtures,
+  PlaywrightTestArgs & PlaywrightTestOptions & ThreeTestFixtures,
   PlaywrightWorkerArgs & PlaywrightWorkerOptions
-> = base.extend<ThreeFixtures>({
+> = base.extend<ThreeTestFixtures>({
+  autoLoadSceneScripts: [
+    async ({ page }, use) => {
+      await page.addInitScript({
+        path: objectGeneratorsPath,
+      });
+
+      use("autoLoadSceneScripts");
+    },
+    { scope: "test", auto: true },
+  ],
+
   scene: async ({ page }, use) => {
-    await page.addInitScript({
-      path: objectGeneratorsPath,
-    });
-    // Ensure that the init script is available
-    await page.reload();
     const scene = new Scene(page);
     await use(scene);
   },
