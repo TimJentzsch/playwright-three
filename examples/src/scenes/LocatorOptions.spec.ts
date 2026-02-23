9ca608be501f5a75823333c6f2cd1ac39a2cee76
Tim Jentzsch <tim.jentzsch@faro.com>
2026-02-23 10:30:04 +0100

    Fix browser closing before test finished

--- examples/src/scenes/LocatorOptions.spec.ts
+++ examples/src/scenes/LocatorOptions.spec.ts
@@ -6,17 +6,17 @@ test.beforeEach(async ({ page }) => {
 
 test("has 0 boxes at depth 1", async ({ scene }) => {
   // Only groups at that level
-  expect(scene.getByName("box", { maxDepth: 1 })).toHaveCountInScene(0);
+  await expect(scene.getByName("box", { maxDepth: 1 })).toHaveCountInScene(0);
 });
 
 test("has 1 box at depth 2", async ({ scene }) => {
-  expect(scene.getByName("box", { maxDepth: 2 })).toHaveCountInScene(1);
+  await expect(scene.getByName("box", { maxDepth: 2 })).toHaveCountInScene(1);
 });
 
 test("has 3 boxes at depth 3", async ({ scene }) => {
-  expect(scene.getByName("box", { maxDepth: 3 })).toHaveCountInScene(3);
+  await expect(scene.getByName("box", { maxDepth: 3 })).toHaveCountInScene(3);
 });
 
 test("has 3 boxes at unlimited depth", async ({ scene }) => {
-  expect(scene.getByName("box")).toHaveCountInScene(3);
+  await expect(scene.getByName("box")).toHaveCountInScene(3);
 });
