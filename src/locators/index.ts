b0ef9ad6984da7968ab44f6968ee8b959306bab4
Tim Jentzsch <tim.jentzsch@faro.com>
2026-03-06 17:23:35 +0100

    Add exports for material and geometry locators

--- src/locators/index.ts
+++ src/locators/index.ts
@@ -2,3 +2,5 @@ export * from "./locatorApi";
 export * from "./locatorContext";
 export * from "./objectLocator";
 export * from "./sceneLocator";
+export * from "./materialLocator";
+export * from "./geometryLocator";
