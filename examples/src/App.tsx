e05c7499ca847931aec82ab49ebf6d0139d8d3e4
Tim Jentzsch <tim.jentzsch@faro.com>
2026-03-10 11:58:13 +0100

    Fix formatting

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -4,28 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
 import { ExposeThree } from "./ExposeThree";
 import { Handles, HANDLES_ROUTE as HANDLES_PATH } from "./pages/Handles";
-import {
-  Hierarchy,
-  HIERARCHY_ROUTE as HIERARCHY_PATH,
-} from "./pages/Hierarchy";
-import {
-  AUTO_WAITING_ROUTE as AUTO_WAITING_PATH,
-  AutoWaiting,
-} from "./pages/AutoWaiting";
+import { Hierarchy, HIERARCHY_ROUTE as HIERARCHY_PATH } from "./pages/Hierarchy";
+import { AUTO_WAITING_ROUTE as AUTO_WAITING_PATH, AutoWaiting } from "./pages/AutoWaiting";
 import {
   LOCATOR_OPTIONS_ROUTE as LOCATOR_OPTIONS_PATH,
   LocatorOptions,
 } from "./pages/LocatorOptions";
-import {
-  Transform,
-  TRANSFORM_ROUTE as TRANSFORM_PATH,
-} from "./pages/Transform";
+import { Transform, TRANSFORM_ROUTE as TRANSFORM_PATH } from "./pages/Transform";
 import { USER_DATA_ROUTE as USER_DATA_PATH, UserData } from "./pages/UserData";
 import { Color, COLOR_ROUTE as COLOR_PATH } from "./pages/Color";
-import {
-  NAME_AND_TYPE_ROUTE as NAME_AND_TYPE_PATH,
-  NameAndType,
-} from "./pages/NameAndType";
+import { NAME_AND_TYPE_ROUTE as NAME_AND_TYPE_PATH, NameAndType } from "./pages/NameAndType";
 import { Count, COUNT_ROUTE as COUNT_PATH } from "./pages/Count";
 
 export default function App() {
